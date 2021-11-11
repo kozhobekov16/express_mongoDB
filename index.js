7840bd5fc6c18fff3b10bba3ff57ae1c1500310e
unknown <you@example.com>
2021-11-11 15:29:18 +0600

    mongo

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(todoRoutes)
 
 async function start(){
--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
 const router = Router()
-
-router.get('/', (req, resp) =>{
+let Todo = require('../models/Todo')
+router.get('/', async (req, resp) =>{
+    const todos = await Todo.find({})
     resp.render('index', {
         title: 'Todo List',
-        isIndex: true
+        isIndex: true,
+        
     })
 })
 router.get('/create', (req, res) => {
@@ -12,5 +14,12 @@ router.get('/create', (req, res) => {
         title: 'Create ToDo',
         isCreate: true
     })
+})
+router.post('./create', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    })
+    await todo.save()
+    res.redirect('/')
 })
 module.exports = router
